4df64581102df32dfb8213ff22547b66ae98975c
Jay <arinjayjoshi224@gmail.com>
2026-09-03 15:17:46 +0530

    Revert "Add files via upload"

--- Menu.py
+++
@@ -1,9 +0,0 @@
-class menu:
-    def __init__(self, name, options):
-        self.name = name
-        self.options = options
-
-    def display(self):
-        print(f"Menu: {self.name}")
-        for index, option in enumerate(self.options, start=1):
-            print(f"{index}. {option}")
--- main.py
+++
@@ -1,4 +0,0 @@
-import Menu
-
-menu = Menu.menu("Main Menu", ["Option 1", "Option 2", "Option 3"])
-menu.display()
